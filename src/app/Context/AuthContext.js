aad994190ae94352d60cfbb56473f9389fb4b9f4
attanaseem306 <attanaseem306@gmail.com>
2023-09-30 04:06:30 -0700

    first commit

---
+++ src/app/Context/AuthContext.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+    onAuthStateChanged,
+    getAuth,
+} from 'firebase/auth';
+import { useState,useContext,useEffect,createContext } from 'react';
+import firebase_app from '@/app/config/firebase';
+
+const auth = getAuth(firebase_app);
+
+export const AuthContext = createContext({});
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({
+    children,
+}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user }}>
+            {loading ? <div>Loading...</div> : children}
+        </AuthContext.Provider>
+    );
+};
